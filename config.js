f1e49db9c8c7e5311031a98c28331f2e475978a2
cube-red <61773390+cube-red@users.noreply.github.com>
2021-09-01 23:51:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 // @ts-check
 
 const config = {
-  endpoint: "<Your Azure Cosmos account URI>",
+  endpoint: "https://cosmosdb111.documents.azure.com:443/",
   key: "<Your Azure Cosmos account key>",
   databaseId: "Tasks",
   containerId: "Items",
   partitionKey: { kind: "Hash", paths: ["/category"] }
+  
 };
 
 module.exports = config;
